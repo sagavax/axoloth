b3dd039c13fe5b7036c0049ef0953b77e1e0437d
Tomas Misura <tmisura@gmail.com>
2025-03-12 20:39:24 +0100

    removed unused files

--- prod/include/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-    //define('APP_PATH','https://axoloth.tmisura.sk/');
-    define('APP_PATH','http://localhost/axoloth/prod/');
-    define("APP_SHORT_NAME","Axoloth");
-    define('APP_LONG_NAME','Axoloth');
-    include("dbconnect.php");
--- prod/tasks/task_note_remove.php
+++
@@ -1,5 +0,0 @@
-<?php
-    include("../include/dbconnect.php");
-
-
-    $note_id = 
