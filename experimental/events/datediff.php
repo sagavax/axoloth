228be442ddeea44d7c866af0ee148e3503657480
Tomas Misura <tmisura@gmail.com>
2025-03-13 00:28:16 +0100

    removed useless files

--- experimental/events/datediff.php
+++
@@ -1,9 +0,0 @@
-<?php
-	function dateDiff ($d1, $d2) {
-	// Return the number of days between the two dates:
-
-  	return round(abs(strtotime($d1)-strtotime($d2))/86400);
-
-	}  // end function dateDiff
-
-?>	
--- experimental/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();// Zapneme session
-//session_destroy();// Smažeme všechna session
-header("location: index.php"); // Přsesmeruje na přihlašovací stránku
-?>
