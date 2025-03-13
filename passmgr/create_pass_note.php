d3337794d2f3ce3f67ecb410f0657323e8262767
Tomas Misura <tmisura@gmail.com>
2025-03-13 14:07:58 +0100

    Update create_pass_note.php
    fixing the code reflecting of database changes

--- passmgr/create_pass_note.php
+++ passmgr/create_pass_note.php
@@ -8,7 +8,7 @@ global $con;
 $note_text = mysqli_real_escape_string($con, $_POST['note_text']);
 $pass_id = $_POST['pass_id'];
 
-$create_note = "INSERT INTO tblpassword_notes (pass_id,note_text,updated_date) VALUES ($pass_id, '$note_text',NOW())";
+$create_note = "INSERT INTO tblpassword_notes (pass_id,note_text,created_date) VALUES ($pass_id, '$note_text',NOW())";
 $result = mysqli_query($con, $create_note) or die("MySQLi ERROR: " . mysqli_error($con));
 
 $newest_id = GetLatestPasswordNote();
